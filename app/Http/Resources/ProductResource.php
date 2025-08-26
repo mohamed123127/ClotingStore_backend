dd3ac2a980fd4b92d33c83cfd14c6ae7f326d812
mohamed123127 <mohamedlouahchi9@gmail.com>
2025-08-26 23:14:04 +0100

    micro update

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -19,7 +19,7 @@ class ProductResource extends JsonResource
             'id'    => $this->id,
             'name'  => $this->name,
             'description' => $this->description,
-            'price' => $this->price,
+            'price' => number_format($this->price,0),
             'preview_image' => $this->preview_image,
         ];
     }
