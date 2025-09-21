02a4ccfe84fec737aa08b60eae295630759b3f47
mohamed123127 <mohamedlouahchi9@gmail.com>
2025-09-21 17:37:55 +0100

    add sentry config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(VerifyApiKey::class);
+        $middleware->append(\Sentry\Laravel\Tracing\Middleware::class); // Performance tracing
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
     })->create();
